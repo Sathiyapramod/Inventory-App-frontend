8d23acdaaf94453a0f1776bca3d82f1c1e046100
Sathiyapramod <sathiyapramod22@gmail.com>
2023-04-08 12:23:51 +0530

    aded pagination

--- src/components/billing/Billing.js
+++ src/components/billing/Billing.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { backendAPI } from "../General";
+import { Pagination } from "@mui/material";
 import ReceiptIcon from "@mui/icons-material/Receipt";
 import "./Billing.css";
 
@@ -10,11 +11,24 @@ function Billing() {
   const navigate = useNavigate();
   const [bills, setBills] = useState([]);
   const [billdetails, setBilldetail] = useState({});
+  const [currentPage, setCurrentPage] = useState(1);
 
-  useEffect(() => {
+  const handleChange = (event, page) => {
+    setCurrentPage(page);
+    console.log(page);
+    let startIndex = (page - 1) * 5;
+    let endIndex = startIndex + 4;
     fetch(`${backendAPI}/billing`)
       .then((response) => response.json())
-      .then((result) => setBills(result));
+      .then((result) => {
+        // console.log(result.slice(startIndex, endIndex));
+        setBills(result.slice(startIndex, endIndex));
+      });
+  };
+  useEffect(() => {
+    // fetch(`${backendAPI}/billing`)
+    //   .then((response) => response.json())
+    //   .then((result) => setBills(result));
   }, []);
   return (
     <div>
@@ -54,7 +68,7 @@ function Billing() {
           </thead>
           <tbody>
             {bills.length !== 0 ? (
-              bills.slice(0, 10).map((billItem, index) => {
+              bills.map((billItem, index) => {
                 return (
                   <tr key={index}>
                     <td>{index + 1}</td>
@@ -97,6 +111,14 @@ function Billing() {
           </tbody>
         </table>
       </div>
+      <Pagination
+        count={2}
+        color="secondary"
+        className="d-flex flex-row justify-content-center mx-auto"
+        page={currentPage}
+        defaultPage={1}
+        onChange={handleChange}
+      />
     </div>
   );
 }
@@ -113,129 +135,135 @@ function ViewBilldetail(props) {
       aria-labelledby="staticBackdropLabel"
       aria-hidden="true"
     >
-      {props.billdetails && <div className="modal-dialog modal-fullscreen">
-        <div className="modal-content">
-          <div className="modal-header hide-on-plot">
-            {/* <h1 className="modal-title fs-5" id="staticBackdropLabel">
+      {props.billdetails && (
+        <div className="modal-dialog modal-fullscreen">
+          <div className="modal-content">
+            <div className="modal-header hide-on-plot">
+              {/* <h1 className="modal-title fs-5" id="staticBackdropLabel">
               Modal title
             </h1> */}
-            <button
-              type="button"
-              className="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div className="modal-body">
-            <div className="mx-auto fs-5">Tax Invoice</div>
-            <br />
-            <div className="mx-auto fs-5 d-flex flex-column align-items-center justify-content-center">
-              <span className="fs-2 fw-bolder">Rampage</span>
-              <span className="fs-4">#32, Godown Street</span>
-              <span className="fs-4">George Town, Chennai - 600 001</span>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
             </div>
-            <br />
-            <div className="d-flex flex-row justify-content-between align-items-center ps-2 pe-2">
-              <div>
-              <div className="d-flex flex-row justify-content-start align-items-center gap-2">
-              <label>Customer Name</label>
-                  <span className="fs-3">{"BIL".concat(props.billdetails._id)}</span>
+            <div className="modal-body">
+              <div className="mx-auto fs-5">Tax Invoice</div>
+              <br />
+              <div className="mx-auto fs-5 d-flex flex-column align-items-center justify-content-center">
+                <span className="fs-2 fw-bolder">Rampage</span>
+                <span className="fs-4">#32, Godown Street</span>
+                <span className="fs-4">George Town, Chennai - 600 001</span>
               </div>
-                <div className="d-flex flex-row justify-content-start align-items-center gap-2">
-                  <label>Customer Name</label>
-                  <span className="fs-3">{props.billdetails.customerName}</span>
+              <br />
+              <div className="d-flex flex-row justify-content-between align-items-center ps-2 pe-2">
+                <div>
+                  <div className="d-flex flex-row justify-content-start align-items-center gap-2">
+                    <label>Customer Name</label>
+                    <span className="fs-3">
+                      {"BIL".concat(props.billdetails._id)}
+                    </span>
+                  </div>
+                  <div className="d-flex flex-row justify-content-start align-items-center gap-2">
+                    <label>Customer Name</label>
+                    <span className="fs-3">
+                      {props.billdetails.customerName}
+                    </span>
+                  </div>
+                  <div className="d-flex flex-row justify-content-start align-items-center gap-2">
+                    <label>Bill Mode</label>
+                    <span className="fs-3">{props.billdetails.billMode}</span>
+                  </div>
+                  <div className="d-flex flex-row justify-content-start align-items-center gap-2">
+                    <label>GST Number</label>
+                    <span className="fs-3">{props.billdetails.billMode}</span>
+                  </div>
                 </div>
-                <div className="d-flex flex-row justify-content-start align-items-center gap-2">
-                  <label>Bill Mode</label>
-                  <span className="fs-3">{props.billdetails.billMode}</span>
-                </div>
-                <div className="d-flex flex-row justify-content-start align-items-center gap-2">
-                  <label>GST Number</label>
-                  <span className="fs-3">{props.billdetails.billMode}</span>
+                <div>
+                  <div className="d-flex flex-row justify-content-end align-items-center gap-2">
+                    <label>Billing Address</label>
+                    <span className="fs-3">Address</span>
+                  </div>
+                  <div className="d-flex flex-row justify-content-end align-items-center gap-2">
+                    <label>Shipping Address</label>
+                    <span className="fs-3">Address</span>
+                  </div>
                 </div>
               </div>
-              <div>
-                <div className="d-flex flex-row justify-content-end align-items-center gap-2">
-                  <label>Billing Address</label>
-                  <span className="fs-3">Address</span>
-                </div>
-                <div className="d-flex flex-row justify-content-end align-items-center gap-2">
-                  <label>Shipping Address</label>
-                  <span className="fs-3">Address</span>
-                </div>
+              <div className="d-flex flex-row justify-content-center align-items-start">
+                <table className="table container table-bordered table-hover fs-5">
+                  <thead>
+                    <tr>
+                      {[
+                        "#",
+                        "Item Name",
+                        "UOM",
+                        "Quantity",
+                        "Rate",
+                        "Amount",
+                      ].map((element, index) => {
+                        return <th key={index}>{element}</th>;
+                      })}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {props.billdetails.items &&
+                      props.billdetails.items.map((item, index) => {
+                        return (
+                          <tr key={index}>
+                            <td>{index + 1}</td>
+                            <td>{item.name}</td>
+                            <td>{item.units}</td>
+                            <td>{item.qty}</td>
+                            <td>{item.rate}</td>
+                            <td>{item.total}</td>
+                          </tr>
+                        );
+                      })}
+                  </tbody>
+                </table>
+              </div>
+              <div className="d-flex flex-column justify-content-center align-items-center position-absolute bottom-0 end-0 pe-4">
+                <span className="d-flex flex-row justify-content-end align-items-end gap-2">
+                  <label className="form-label">Gross Total</label>
+                  <span className="fs-4">${props.billdetails.grossTotal}</span>
+                </span>
+                <span className="d-flex flex-row justify-content-end align-items-end gap-2">
+                  <label className="form-label col">GST </label>
+                  <span className="fs-4">${props.billdetails.gst}</span>
+                </span>
+                <span className="d-flex flex-row justify-content-end align-items-end gap-2">
+                  <label className="form-label col">Total Amount </label>
+                  <span className="fs-4">${props.billdetails.NetTotal}</span>
+                </span>
+              </div>
+              <div className="position-absolute bottom-0 start-0 ps-3 fst-italic">
+                This is a System Generated Invoice, Hence Signature is not
+                required.
               </div>
             </div>
-            <div className="d-flex flex-row justify-content-center align-items-start">
-              <table className="table container table-bordered table-hover fs-5">
-                <thead>
-                  <tr>
-                    {[
-                      "#",
-                      "Item Name",
-                      "UOM",
-                      "Quantity",
-                      "Rate",
-                      "Amount",
-                    ].map((element, index) => {
-                      return <th key={index}>{element}</th>;
-                    })}
-                  </tr>
-                </thead>
-                <tbody>
-                  {props.billdetails.items &&
-                    props.billdetails.items.map((item, index) => {
-                      return (
-                        <tr key={index}>
-                          <td>{index + 1}</td>
-                          <td>{item.name}</td>
-                          <td>{item.units}</td>
-                          <td>{item.qty}</td>
-                          <td>{item.rate}</td>
-                          <td>{item.total}</td>
-                        </tr>
-                      );
-                    })}
-                </tbody>
-              </table>
-            </div>
-            <div className="d-flex flex-column justify-content-center align-items-center position-absolute bottom-0 end-0 pe-4">
-              <span className="d-flex flex-row justify-content-end align-items-end gap-2">
-                <label className="form-label">Gross Total</label>
-                <span className="fs-4">${props.billdetails.grossTotal}</span>
-              </span>
-              <span className="d-flex flex-row justify-content-end align-items-end gap-2">
-                <label className="form-label col">GST </label>
-                <span className="fs-4">${props.billdetails.gst}</span>
-              </span>
-              <span className="d-flex flex-row justify-content-end align-items-end gap-2">
-                <label className="form-label col">Total Amount </label>
-                <span className="fs-4">${props.billdetails.NetTotal}</span>
-              </span>
-            </div>
-            <div className="position-absolute bottom-0 start-0 ps-3 fst-italic">
-              This is a System Generated Invoice, Hence Signature is not
-              required.
-            </div>
-          </div>
-          <div className="modal-footer">
-            <button
-              type="button"
-              className="btn btn-secondary hide-on-plot"
-              data-bs-dismiss="modal"
-            >
-              Close
-            </button>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary hide-on-plot"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
 
-            <button
-              type="button"
-              className="btn btn-primary hide-on-plot"
-              onClick={handlePrint}
-            >
-              Print
-            </button>
+              <button
+                type="button"
+                className="btn btn-primary hide-on-plot"
+                onClick={handlePrint}
+              >
+                Print
+              </button>
+            </div>
           </div>
         </div>
-      </div>}
+      )}
     </div>
   );
 }
